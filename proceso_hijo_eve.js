be979486287fd116b4d12ffe0ab4ff524425b904
Jesus Garcia <jssgarciar@gmail.com>
2019-04-19 00:10:01 +0200

    Spawn! :muscle:

---
+++ proceso_hijo_eve.js
@@ -0,0 +1,8 @@
+const {spawn} = require('child_process');
+const ping = spawn('ping',['osweekends.com']);
+
+ping.stdout.on('data', (msg) => console.log(`[stdout][${new Date().getTime()}]: ${msg}`));
+
+ping.stderr.on('data', (data) => console.log(`stderr: ${data}`));
+
+ping.on('close', (code) => console.log(`Child process exited with code: ${code}`));
